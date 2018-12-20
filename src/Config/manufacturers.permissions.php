0f8992d703cc0383eaeab04cae291bbabaafb91e
enicaZ <45054493+enicaZ@users.noreply.github.com>
2018-12-20 15:28:04 +0800

    Create manufacturers.permissions.php

---
+++ src/Config/manufacturers.permissions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: Warlof Tutsimo <loic.leuilliot@gmail.com>
+ * Date: 15/06/2016
+ * Time: 22:02
+ */
+return [
+    'manufacturers' => [
+        'view',
+        'create',
+        'doctrineview',
+        'reportview'
+    ]
+];
