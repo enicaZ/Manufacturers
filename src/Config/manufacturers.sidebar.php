6af755526b7460785bf2a0930376da2a8cf53be5
enicaZ <45054493+enicaZ@users.noreply.github.com>
2018-12-20 15:29:15 +0800

    Create manufacturers.sidebar.php

---
+++ src/Config/manufacturers.sidebar.php
@@ -0,0 +1,32 @@
+<?php 
+return [
+    'doctrine' => [
+        'name' => 'Doctrines & Fittings',
+        'permission' => 'fitting.doctrineview',
+        'route_segment' => 'fitting',
+        'icon' => 'fa-rocket',
+        'entries'       => [
+            'fitting' => [
+                'name' => 'Fittings',
+                'icon' => 'fa-rocket',
+                'route_segment' => 'fitting',
+                'route' => 'fitting.view',
+                'permission' => 'fitting.view'
+            ],
+            'doctrine' => [
+                'name' => 'Doctrine',
+                'icon' => 'fa-list',
+                'route_segment' => 'fitting',
+                'route' => 'fitting.doctrineview',
+                'permission' => 'fitting.doctrineview'
+            ],
+            'doctrinereport' => [
+                'name' => 'Doctrine Report',
+                'icon' => 'fa-pie-chart',
+                'route_segment' => 'fitting',
+                'route' => 'fitting.doctrinereport',
+                'permission' => 'fitting.reportview'
+            ],
+        ]
+    ]
+];
